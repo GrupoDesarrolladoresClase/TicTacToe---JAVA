5584555eaaac9418ec71ab0e2c9ea7006d683b88
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-06-02 13:04:52 +0100

    Creates gridLayout in Board and add images in resource folder

--- src/com/studentdevelopers/tictactoe/view/BoardDisplay.java
+++ src/com/studentdevelopers/tictactoe/view/BoardDisplay.java
@@ -1,18 +1,18 @@
 package com.studentdevelopers.tictactoe.view;
 
 import javax.swing.*;
-
-import static java.awt.Color.BLUE;
+import java.awt.*;
 
 public class BoardDisplay extends JPanel {
 
     public BoardDisplay() {
         super();
-        setBackground(BLUE);
+        setLayout(new GridLayout(3, 3));
         createCells();
     }
 
     private void createCells() {
-        
+        for (int i = 0; i < 9; i++)
+            add(new JButton());
     }
 }
