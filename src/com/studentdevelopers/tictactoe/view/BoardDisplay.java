e9566750f9d936daec526559ebbfda9650fd04bc
Ronny Ancorini Rodríguez <Ronnygx@gmail.com>
2015-06-14 21:11:23 +0100

    Change Constructor to receive Operations Maps

--- src/com/studentdevelopers/tictactoe/view/BoardDisplay.java
+++ src/com/studentdevelopers/tictactoe/view/BoardDisplay.java
@@ -2,15 +2,22 @@ package com.studentdevelopers.tictactoe.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Map;
 
 import static java.awt.Color.decode;
 
 public class BoardDisplay extends JPanel {
 
-    public BoardDisplay() {
+    private Map<String, CellButtonOperator> operationsMap;
+
+    public BoardDisplay(Map<String, CellButtonOperator> operationsMap) {
         super();
+        this.operationsMap = operationsMap;
         setBackground(decode("#D9E9E9"));
         setLayout(new GridLayout(3, 3));
+        addButtons();
+    }
+
     }
 
     public void addButton(CellButton button) {
