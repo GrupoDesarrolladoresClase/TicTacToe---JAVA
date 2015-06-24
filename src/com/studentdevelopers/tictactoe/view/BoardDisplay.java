86ab00f0a567036448e96e368a37dc56c7f0e78d
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-06-24 01:40:24 +0100

    Refactor: remove hashmap field and convert to local

--- src/com/studentdevelopers/tictactoe/view/BoardDisplay.java
+++ src/com/studentdevelopers/tictactoe/view/BoardDisplay.java
@@ -1,26 +1,28 @@
 package com.studentdevelopers.tictactoe.view;
 
+import com.studentdevelopers.tictactoe.view.operators.CellOperator;
+
 import javax.swing.*;
 import java.awt.*;
-import java.util.Map;
 
 import static java.awt.Color.decode;
 
 public class BoardDisplay extends JPanel {
 
-    private Map<String, CellButtonOperator> operationsMap;
-
-    public BoardDisplay(Map<String, CellButtonOperator> operationsMap) {
+    public BoardDisplay(CellOperator cellOperator) {
         super();
-        this.operationsMap = operationsMap;
-        setBackground(decode("#D9E9E9"));
-        setLayout(new GridLayout(3, 3));
-        addButtons();
+        setUpBoard();
+        addCells(cellOperator);
     }
 
-    private void addButtons() {
+    private void setUpBoard() {
+        setBackground(decode("#D9E9E9"));
+        setLayout(new GridLayout(3, 3));
+    }
+
+    private void addCells(CellOperator cellOperator) {
         for (int i = 1; i <= 9; i++)
-            addButton(new CellButton(i, operationsMap.get("Cell " + i)));
+            addButton(new CellButton(i, cellOperator));
     }
 
     public void addButton(CellButton button) {
