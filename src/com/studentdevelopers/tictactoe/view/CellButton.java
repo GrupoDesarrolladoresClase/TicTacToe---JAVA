61607626b9a01cffd10e0f022a8fe4092b53ebae
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-06-24 01:38:10 +0100

    Refactor: rename field

--- src/com/studentdevelopers/tictactoe/view/CellButton.java
+++ src/com/studentdevelopers/tictactoe/view/CellButton.java
@@ -2,22 +2,23 @@ package com.studentdevelopers.tictactoe.view;
 
 import com.studentdevelopers.tictactoe.model.board.CellState;
 import com.studentdevelopers.tictactoe.model.helpers.Observer;
+import com.studentdevelopers.tictactoe.view.operators.CellOperator;
 
 import javax.swing.*;
 
 public class CellButton extends JButton implements Observer {
 
     private final int ID;
-    private final CellButtonOperator operator;
+    private final CellOperator operator;
 
-    public CellButton(int id, CellButtonOperator operator) {
+    public CellButton(int id, CellOperator operator) {
         super();
         ID = id;
         this.operator = operator;
         buttonSetUp(operator);
     }
 
-    private void buttonSetUp(CellButtonOperator operator) {
+    private void buttonSetUp(CellOperator operator) {
         setFocusPainted(false);
         operator.addObserverToCell(ID, this);
         addActionListener(event -> operator.markCell(ID));
