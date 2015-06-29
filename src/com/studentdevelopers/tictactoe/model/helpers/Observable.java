c699210bad31a38ac7569782c2866ba9d814d4d4
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-06-29 13:37:18 +0100

    Refactor: remove unused method

--- src/com/studentdevelopers/tictactoe/model/helpers/Observable.java
+++ src/com/studentdevelopers/tictactoe/model/helpers/Observable.java
@@ -7,7 +7,7 @@ public abstract class Observable {
     public ArrayList<Observer> observers = new ArrayList<>();
 
     public abstract void addObserver(Observer observer);
-    public abstract void deleteObserver(Observer observer);
+
     public abstract void notifyObservers();
 
 }
